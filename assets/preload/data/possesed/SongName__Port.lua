f498744f26e05ca0dbf56d0aef4521a4250b1de8
DemonFeverYT <110266652+DemonFeverYT@users.noreply.github.com>
2022-09-22 12:19:21 -0300

    Add files via upload

---
+++ assets/preload/data/possesed/SongName__Port.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', songName .. " | Ported by DemonFever", 0, -2, 550); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 40);
+    addLuaText('watermark');
+end
